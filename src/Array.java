8524239494da1af95f68e1c34802ca6ffb8f1668
saranyaparthiban <saranyanava07@gmail.com>
2019-12-23 14:26:43 +0530

    Array1

--- src/Array.java
+++ src/Array.java
@@ -2,16 +2,16 @@
 public class Array {
 
 public static void main(String[] args) {
-	int arr [] = new int[5];
-	arr [0] = 5;
-	arr [1]= 10;
-	arr [2]= 15;
-	arr [3]=20;
-	arr [4]=25;
+	int arr1 [] = new int[5];
+	arr1 [0] = 15;
+	arr1 [1]= 100;
+	arr1 [2]= 150;
+	arr1 [3]=200;
+	arr1 [4]=250;
 			
-System.out.println(arr[2]);
+System.out.println(arr1[2]);
 
-for (int i : arr) {
+for (int i : arr1) {
 	System.out.println(i);
 }
 	}
